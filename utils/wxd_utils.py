effeef0707c07fd8528ac05bdb2564bd5aa8eaa2
Wilfried Hoge <wilfried@hoge-uebler.de>
2024-05-30 13:27:01 +0200

    data load

--- utils/wxd_utils.py
+++ utils/wxd_utils.py
@@ -14,10 +14,27 @@ def load_conf():
         "password" : os.getenv("LH_PW", "password"),
         "lh_port": os.getenv("LH_PORT", "8443"),
         "lh_cert": os.getenv("LH_CERT", "/wxd-install/ibm-lh-dev/localstorage/volumes/infra/tls/cert.crt"),
+        "lh_schema": os.getenv("LH_SCHEMA", "tiny"),
+        "lh_catalog": os.getenv("LH_CATALOG", "tpch"),
         "milvus_port": os.getenv("MILVUS_PORT", "19530")
     }
 
     return(config)
 
 def connect_wxd(config):
-    return
+
+    import ssl
+    import urllib3
+    import os
+    from sqlalchemy import create_engine
+    urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning) # disable https warning
+
+    quick_engine = create_engine(
+        f"presto://{config.lh_user}:{config.password}@{config.host}:{config.lh_port}/{config.lh_catalog}/{config.lh_schema}",
+        connect_args={
+            'protocol': 'https', 
+            'requests_kwargs': {'verify': ssl.CERT_NONE }
+            }
+        )
+
+    return quick_engine
