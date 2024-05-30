e3e639464bd627ad49530c7329242b3ce20ac6a4
Wilfried Hoge <wilfried@hoge-uebler.de>
2024-05-30 13:03:02 +0200

    config new

--- utils/wxd_utils.py
+++ utils/wxd_utils.py
@@ -6,11 +6,17 @@ def load_conf():
 
     # Load config from environment
     config = {
-        api_key : os.getenv("API_KEY", None),
-        ibm_cloud_url : os.getenv("IBM_CLOUD_URL", None),
-        project_id : os.getenv("PROJECT_ID", None),
+        "api_key" : os.getenv("API_KEY", None),
+        "ibm_cloud_url" : os.getenv("IBM_CLOUD_URL", None),
+        "project_id" : os.getenv("PROJECT_ID", None),
+        "host" : os.getenv("LH_HOST_NAME", "localhost"),
+        "user" : os.getenv("LH_USER", "ibmlhadmin"),
+        "password" : os.getenv("LH_PW", "password"),
+        "lh_port": os.getenv("LH_PORT", "8443"),
+        "lh_cert": os.getenv("LH_CERT", "/wxd-install/ibm-lh-dev/localstorage/volumes/infra/tls/cert.crt"),
+        "milvus_port": os.getenv("MILVUS_PORT", "19530")
     }
-    
+
     return(config)
 
 def connect_wxd(config):
