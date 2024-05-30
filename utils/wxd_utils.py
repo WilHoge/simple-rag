4d9215b594bddb91c0b042d0294c451db63dcf95
Wilfried Hoge <wilfried@hoge-uebler.de>
2024-05-30 15:07:59 +0200

    lab query

--- utils/wxd_utils.py
+++ utils/wxd_utils.py
@@ -38,3 +38,53 @@ def connect_wxd(config):
         )
 
     return quick_engine
+
+def get_token(conf):
+
+    from ibm_cloud_sdk_core import IAMTokenManager
+
+    access_token = IAMTokenManager(
+        apikey = api_key,
+        url = "https://iam.cloud.ibm.com/identity/token"
+    ).get_token()
+
+    return access_token
+
+def load_model(model_id)
+    #        model_id='meta-llama/llama-2-70b-chat'
+    #        model_id='mistralai/mixtral-8x7b-instruct-v01'
+
+    from ibm_watson_machine_learning.foundation_models import Model
+    from ibm_watson_machine_learning.metanames import GenTextParamsMetaNames as GenParams
+
+    creds = {
+        "url": conf["ibm_cloud_url"],
+        "apikey": conf["api_key"] 
+    }
+
+    # Model Parameters
+    params = {
+        GenParams.DECODING_METHOD: "greedy",
+        GenParams.MIN_NEW_TOKENS: 1,
+        GenParams.MAX_NEW_TOKENS: 500,
+        GenParams.TEMPERATURE: 0,
+    }
+
+    model = Model(model_id=model_id, 
+        params=params, credentials=creds, 
+        project_id=project_id
+    )
+
+    return model
+
+# Prompt LLM
+def ask_llm(prompt, model):
+        response = model.generate_text(prompt)
+        #print(f"Question: {question_text}{response}")
+        return response
+
+def make_prompt(context, question_text):
+    context = "\n\n".join(context)
+    return (f"{context}\n\nPlease answer a question using this text. "
+          + f"If the question is unanswerable, say \"unanswerable\"."
+          + f"\n\nQuestion: {question_text}")
