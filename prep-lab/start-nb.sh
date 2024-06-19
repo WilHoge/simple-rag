897f5e33e4954f1fcb7ea2e2f0413937a4acf83f
Wilfried Hoge <WilHoge@users.noreply.github.com>
2024-06-19 10:21:48 +0200

    reset nb with kill

--- prep-lab/start-nb.sh
+++ prep-lab/start-nb.sh
@@ -1,3 +1,6 @@
+# kill previous instances of jupyter notebooks
+kill $(ps aux|fgrep bin/jupyter-notebook|awk '{print $2}')
+
 # start jupyter notebooks
 . ~labuser/venv/bin/activate
-nohup jupyter notebook > ~labuser/simple-rag/logs/jupyter-notebook.log 2>~labuser/simple-rag/logs/jupyter-notebook.err & 
+nohup jupyter notebook 1>~labuser/simple-rag/logs/jupyter-notebook.log 2>~labuser/simple-rag/logs/jupyter-notebook.err & 
