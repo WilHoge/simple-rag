5772796922efedac58bd00b84401ddba7b0ba430
Wilfried Hoge <WilHoge@users.noreply.github.com>
2024-06-19 09:57:09 +0200

    reset tasks

---
+++ prep-lab/reset-lab.sh
@@ -0,0 +1,8 @@
+# this script initializes the environment and resets the lab
+
+# 1. clean lab directory and reload from git
+sudo rm -rf ~labuser/simple-rag
+sudo su - labuser -c "git clone https://github.com/WilHoge/simple-rag.git"
+
+# 2. install the required python libraries
+sudo su - labuser -c ~labuser/simple-rag/prep-lab/install-python-requirements.sh
